d4eefa8f08b52397fc68d9db3624007ec21a3c3a
twistedwarden <twistedwarden626@gmail.com>
2025-08-04 14:28:01 +0800

    rollback

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api':'https://backend-production-af06.up.railway.app',
+      '/api':'http://localhost:8000',
     },
   },
 });
