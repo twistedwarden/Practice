9aa2cdd7b12ef9bfe350300adc020d885818f36f
twistedwarden <twistedwarden626@gmail.com>
2025-08-05 21:42:38 +0800

    api testing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api':'http://localhost:8000',
+      '/api':'https://practice-production-ff05.up.railway.app',
     },
   },
 });
